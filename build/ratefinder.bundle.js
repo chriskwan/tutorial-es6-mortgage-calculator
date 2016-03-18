f348f24ab1769618da673f2cab2d74923995a8f3
Chris Kwan <chriswkwan@gmail.com>
2016-03-18 08:11:50 -0400

    9) 2. Create a promise and make rate-service-mock

--- build/ratefinder.bundle.js
+++ build/ratefinder.bundle.js
@@ -42,23 +42,55 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 	
-	var url = "rates.json";
+	var _rateServiceMock = __webpack_require__(2);
 	
-	fetch(url).then(function (response) {
-	    return response.json();
-	}).then(function (rates) {
+	var service = _interopRequireWildcard(_rateServiceMock);
+	
+	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+	
+	service.findAll().then(function (rates) {
 	    var html = '';
 	    rates.forEach(function (rate) {
-	        return html += "<tr><td>" + rate.name + "</td><td>" + rate.years + "</td><td>" + rate.rate + "%</td></tr>";
+	        return html += '<tr><td>' + rate.name + '</td><td>' + rate.years + '</td><td>' + rate.rate + '%</td></tr>';
 	    });
 	    document.getElementById("rates").innerHTML = html;
 	}).catch(function (e) {
 	    return console.log(e);
 	});
+
+/***/ },
+/* 1 */,
+/* 2 */
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	var rates = [{
+	    "name": "30 years fixed",
+	    "rate": "13",
+	    "years": "30"
+	}, {
+	    "name": "20 years fixed",
+	    "rate": "2.8",
+	    "years": "20"
+	}];
+	
+	var findAll = exports.findAll = function findAll() {
+	    return new Promise(function (resolve, reject) {
+	        if (rates) {
+	            resolve(rates);
+	        } else {
+	            reject("No rates");
+	        }
+	    });
+	};
 
 /***/ }
 /******/ ]);
--- js/ratefinder.js
+++ js/ratefinder.js
@@ -1,7 +1,6 @@
-let url = "rates.json";
+import * as service from './rate-service-mock';
 
-fetch(url)
-    .then(response => response.json())
+service.findAll()
     .then(rates => {
         let html = '';
         rates.forEach(rate => html += `<tr><td>${rate.name}</td><td>${rate.years}</td><td>${rate.rate}%</td></tr>`);
